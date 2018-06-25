946479daef933941b4be547690f11e4fb846803a
Alejandro Aguilar <alejandro.aguilar.jr@gmail.com>
2018-06-25 23:37:32 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown (total_time)
+  x = total_time
+  while x != 0
+    puts "#{x} SECOND(S)!"
+    x-=1
+  end 
+  return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (total_time)
+  x = total_time
+  while x != 0
+    puts "#{x} SECOND(S)!"
+    x-=1
+    sleep(1)
+  end 
+  return "HAPPY NEW YEAR!"
 end
