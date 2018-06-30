002f79425017c4ec3a59d812035090cff4a5ec84
Scott Tang <scott.q.tang@gmail.com>
2018-06-29 23:41:38 -0500

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
 #write your code here
 
-def countdown
+def countdown(int)
+    while int > 0
+        sleep 5
+        puts "#{int} SECOND(S)!"
+        int-=1
+    end
+    "HAPPY NEW YEAR!"
 end
+
+countdown(12)
+
